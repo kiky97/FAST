bdb021bd15c1105b4d614aa33f92500e13118d4d
kiky97 <146445548+kiky97@users.noreply.github.com>
2026-04-10 14:52:07 +0200

    forth save

--- schemas/story.py
+++ schemas/story.py
@@ -16,4 +16,29 @@ class CompleteStoryNodeResponse(StoryNodeBase):
     options: List[StoryOptionSchema] =[]
     class Config:
         from_attributes = True
+
+
+class StoryBase(BaseModel):
+    title: str
+    session_id:Optional[str] = None
+    class Config:
+        from_attributes = True
+
+class CreateStoryRequest(StoryBase):
+    theme: str
+
+
+class CompleteStoryResponse(StoryBase):
+    id: int
+    created_at: datetime
+    root_node: CompleteStoryNodeResponse
+    all_nodes: Dict[int, CompleteStoryNodeResponse]
+    class Config:
+        from_attributes = True  
+
+
+
+
+
+
         
